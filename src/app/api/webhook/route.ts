a117f3e81ac8065c54828ac4ca79f599c86e50e7
ak0007-code <asahi0107wave@gmail.com>
2024-12-14 12:56:15 -0800

    modify gpt process

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -27,16 +27,16 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: "Invalid signature" }, { status: 401 });
   }
   const events = body.events;
+  const responseText = await handleGptEvent();
   for (const event of events) {
-    await handleLineEvent(event);
+    await handleLineEvent(event, responseText || "");
   }
   return NextResponse.json({ status: "ok" });
 }
 
-async function handleLineEvent(event: WebhookEvent) {
+async function handleLineEvent(event: WebhookEvent, responseText: string) {
   if (event.type === "message" && event.message.type === "text") {
     const userMessage = event.message.text;
-    const responseText = await handleGptEvent();
     const replyText = `🇯🇵 ${userMessage}, ${responseText}`;
     await lineClient.replyMessage({
       replyToken: event.replyToken,
@@ -55,8 +55,30 @@ async function handleLineEvent(event: WebhookEvent) {
 async function handleGptEvent() {
   const chatCompletion = await gptClient.chat.completions.create({
     messages: [{ role: "user", content: "Say this is a test" }],
-    model: "gpt-4o",
+    model: "gpt-3.5-turbo",
   });
   const responseText = chatCompletion.choices[0].message.content;
   return responseText;
+}
+
+export async function GET(req: Request) {
+  try {
+    console.log(process.env.OPENAI_API_KEY);
+    const res = await handleGptEvent();
+    console.log({ res });
+
+    // リクエストのURLからクエリパラメータを取得（例: ?name=ChatGPT）
+    const { searchParams } = new URL(req.url);
+    const name = searchParams.get("name") || "World";
+
+    // レスポンスとしてJSONを返す
+    return NextResponse.json({ message: `Hello, ${name}!` }, { status: 200 });
+  } catch (e) {
+    // エラーハンドリング
+    console.error("APIエラー:", e);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
