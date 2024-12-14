c683cbcac84ae52f45a7c5256eed56e47dcb79b7
ak0007-code <asahi0107wave@gmail.com>
2024-12-14 15:38:19 -0800

    test

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 import { WebhookRequestBody, WebhookEvent, messagingApi } from "@line/bot-sdk";
 const { MessagingApiClient } = messagingApi;
-import crypto from "crypto";
+// import crypto from "crypto";
 import OpenAI from "openai";
 import emojiRegex from "emoji-regex";
 
 const channelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN || "";
-const channelSecret = process.env.LINE_CHANNEL_SECRET || "";
+// const channelSecret = process.env.LINE_CHANNEL_SECRET || "";
 
 const lineClient = new MessagingApiClient({
   channelAccessToken: channelAccessToken,
@@ -18,15 +18,15 @@ const gptClient = new OpenAI({
 
 export async function POST(req: Request) {
   const body = (await req.json()) as WebhookRequestBody;
-  const signature = req.headers.get("X-Line-Signature") || "";
-  const bodyString = JSON.stringify(body);
-  const hash = crypto
-    .createHmac("sha256", channelSecret)
-    .update(bodyString)
-    .digest("base64");
-  if (hash !== signature) {
-    return NextResponse.json({ error: "Invalid signature" }, { status: 401 });
-  }
+  // const signature = req.headers.get("X-Line-Signature") || "";
+  // const bodyString = JSON.stringify(body);
+  // const hash = crypto
+  //   .createHmac("sha256", channelSecret)
+  //   .update(bodyString)
+  //   .digest("base64");
+  // if (hash !== signature) {
+  //   return NextResponse.json({ error: "Invalid signature" }, { status: 401 });
+  // }
   const events = body.events;
   for (const event of events) {
     await handleLineEvent(event);
